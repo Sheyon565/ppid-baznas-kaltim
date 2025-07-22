a792b163dc1db51450e229155f880a57ffafcd32
sendyahmad2323 <sendyahmad415@gmail.com>
2025-07-22 11:06:25 +0800

    update

--- config/settings.py
+++ config/settings.py
@@ -29,12 +29,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "fallback-secret-if-not-set")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
